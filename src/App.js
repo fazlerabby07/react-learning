60ab684e2de0340dbb0ec68451277111821345d6
fazlerabby07 <fazlerabby07@gmail.com>
2020-04-07 19:14:52 +0600

    convert project to JSX, add React-specific ESLint config

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import ReactDOM from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-    return React.createElement('div', { id: 'something-important' }, [
-        React.createElement('h1', {}, 'Adopt Me!'),
-        React.createElement(Pet, {
-            name: 'Luna',
-            animal: 'Dog',
-            breed: 'havanese',
-        }),
-        React.createElement(Pet, {
-            name: 'Pepper',
-            animal: 'Bird',
-            breed: 'Cocktiel',
-        }),
-        React.createElement(Pet, {
-            name: 'Doink',
-            animal: 'cat',
-            breed: 'Mixed',
-        }),
-    ]);
+    return (
+        <div>
+            <h1 id="something-important">Adopt Me!</h1>
+            <Pet name="Luna" animal="Dog" breed="Havanese" />
+            <Pet name="Pepper" animal="Bird" breed="Cocktiel" />
+            <Pet name="Doink" animal="cat" breed="Mixed" />
+        </div>
+    );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
