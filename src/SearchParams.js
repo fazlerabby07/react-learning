9eff0b08422503599e8989a64480e5601fb8f733
Muhammad Hussain <fazlerabby07@gmail.com>
2020-04-08 13:34:17 +0600

    async calls to Petfinder API, update Pet component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
+import Result from './Result';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -8,6 +9,17 @@ const SearchParams = () => {
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
     const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+    const [pets, setPets] = useState([]);
+
+    async function requestPets() {
+        const { animals } = await pet.animals({
+            location,
+            breed,
+            type: animal,
+        });
+
+        setPets(animals || []);
+    }
 
     useEffect(() => {
         setBreeds([]);
@@ -26,7 +38,10 @@ const SearchParams = () => {
     return (
         <div className="search-params">
             <h1>{location}</h1>
-            <form>
+            <form onSubmit={ e => {
+                e.preventDefault();
+                requestPets();
+            }}>
                 <label htmlFor="location">
                     Location
                     <input
@@ -40,6 +55,7 @@ const SearchParams = () => {
                 <BreedDropdown />
                 <button>Submit</button>
             </form>
+            <Result pets={pets}/>
         </div>
     );
 };
